6613257eb2284c8d97282085ee4d11508e46a58e
Loretucci <tuccilore01@gmail.com>
2026-01-04 11:19:40 +0100

    iniziata la funzione solver

--- src/ltv_solver_LQR.py
+++ src/ltv_solver_LQR.py
@@ -24,3 +24,91 @@ def ltv_LQR(AAin, BBin, QQin, RRin, SSin, QQfin, TT, x0, qqin = None, rrin = Non
     - KK (mm x nn x TT) optimal gain sequence
     - PP (nn x nn x TT) riccati matrix
 """
+  try:
+    # check if matrix is (.. x .. x TT) - 3 dimensional array 
+    ns, lA = AAin.shape[1:]
+  except:
+    # if not 3 dimensional array, make it (.. x .. x 1)
+    AAin = AAin[:,:,None]
+    ns, lA = AAin.shape[1:]
+
+  try:  
+    ni, lB = BBin.shape[1:]
+  except:
+    BBin = BBin[:,:,None]
+    ni, lB = BBin.shape[1:]
+
+  try:
+      nQ, lQ = QQin.shape[1:]
+  except:
+      QQin = QQin[:,:,None]
+      nQ, lQ = QQin.shape[1:]
+
+  try:
+      nR, lR = RRin.shape[1:]
+  except:
+      RRin = RRin[:,:,None]
+      nR, lR = RRin.shape[1:]
+
+  try:
+      nSi, nSs, lS = SSin.shape
+  except:
+      SSin = SSin[:,:,None]
+      nSi, nSs, lS = SSin.shape
+
+  # Check dimensions consistency -- safety
+  if nQ != ns:
+    print("Matrix Q does not match number of states")
+    exit()
+  if nR != ni:
+    print("Matrix R does not match number of inputs")
+    exit()
+  if nSs != ns:
+    print("Matrix S does not match number of states")
+    exit()
+  if nSi != ni:
+    print("Matrix S does not match number of inputs")
+    exit()
+
+
+  if lA < TT:
+    AAin = AAin.repeat(TT, axis=2)
+  if lB < TT:
+    BBin = BBin.repeat(TT, axis=2)
+  if lQ < TT:
+    QQin = QQin.repeat(TT, axis=2)
+  if lR < TT:
+    RRin = RRin.repeat(TT, axis=2)
+  if lS < TT:
+    SSin = SSin.repeat(TT, axis=2)
+
+  # Inizialization of the matrices
+  KK = np.zeros((ni, ns, TT))
+  sigma = np.zeros((ni, TT))
+  PP = np.zeros((ns, ns, TT))
+  pp = np.zeros((ns, TT))
+
+  # Renaming the input matrices
+  QQ = QQin
+  RR = RRin
+  SS = SSin
+  QQT = QQfin
+    
+  qq = qqin
+  rr = rrin
+
+  qqT = qqfin
+
+  AA = AAin
+  BB = BBin
+
+  xx = np.zeros((ns, TT))
+  uu = np.zeros((ni, TT))
+
+  xx[:,0] = x0
+  
+  PP[:,:,-1] = QQT
+  pp[:,-1] = qqT
+
+
+  
